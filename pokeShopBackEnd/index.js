7d11d63b1a74b2a976bb87cb75d178a8cf209c51
Beasted179 <clarios179@gmail.com>
2023-02-23 19:35:51 -0800

    CL -b -5

---
+++ pokeShopBackEnd/index.js
@@ -0,0 +1,11 @@
+const PORT = process.env.port || 3000;
+const express = require('express');
+const server = express();
+const apiRouter = require('./api');
+const morgan = require('morgan');
+server.use(morgan('dev'));  
+server.use(express.json())
+server.use('/api', apiRouter);
+server.listen(PORT, () => {
+  console.log('The server is up on port', PORT)
+});
