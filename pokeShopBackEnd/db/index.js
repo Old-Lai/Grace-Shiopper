e665913a789c5d46391bbe9babe48c093f97f5e0
Aldo <amgallegos2016@gmail.com>
2023-02-25 14:15:54 -0800

    Added createUser function

--- pokeShopBackEnd/db/index.js
+++ pokeShopBackEnd/db/index.js
@@ -4,9 +4,26 @@ const { Client } = require('pg');
 const client = new Client()
 
 //db code stuff below
+async function createUser({ username, password, email, isAdmin}) {
+    try {
+        const { rows: [ user ] } = await client.query(`
+        INSERT INTO users(username, password, email, isAdmin) 
+        VALUES($1, $2, $3, $4) 
+        ON CONFLICT (username) DO NOTHING 
+        RETURNING *;
+        `, [username, password, email, isAdmin]);
+
+    return user;
+    } catch (error) {
+    throw error;
+    }
+}
+
 
 
 //export
 module.exports = {
     client,
+    createUser
 }
+
