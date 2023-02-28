e8af4b115998368a12e76d57e080dac32ef4bdab
Henry <henrylai8115@gmail.com>
2023-02-27 21:14:17 -0800

    added createProduct

--- pokeShopBackEnd/db/index.js
+++ pokeShopBackEnd/db/index.js
@@ -4,7 +4,20 @@ const { Client } = require('pg');
 const client = new Client()
 
 //db code stuff below
+async function createProduct({productName, productDes, dollarAmt, stockCount}){
+        try{
+            const {rows:[product]} = await client.query(`
+                INSERT INTO products(name, prodDes, dollarAmt, stockCount)
+                VALUES($1, $2, $3, $4)
+                ON CONFLICT DO NOTHING
+                RETURNING *;
+            `,[productName, productDes, dollarAmt, stockCount])
 
+            return product
+        } catch(e) {
+            throw e
+        }
+}
 
 //export
 module.exports = {
