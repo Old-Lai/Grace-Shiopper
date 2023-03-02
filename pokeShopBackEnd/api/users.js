58e6e2c480f4340e5e820499806324d3e1739022
Beasted179 <clarios179@gmail.com>
2023-03-01 21:18:18 -0800

    /login added

--- pokeShopBackEnd/api/users.js
+++ pokeShopBackEnd/api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const usersRouter = express.Router();
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt")
 const { createUser, updateUser, getUserByUsername } = require("../db");
 usersRouter.use((req, res, next) => {
   console.log("A request is being made to /users");
@@ -10,42 +11,72 @@ usersRouter.post("/register", async (req, res, next) => {
   const { username, password, email } = req.body;
 
   try {
-    if(!username || !password || !email) {
+    if (!username || !password || !email) {
       next({
         name: "MissingParams",
         message: "Something is missing",
       });
+    } else {
+      const _user = await getUserByUsername(username);
+
+      if (_user) {
+        next({
+          name: "UserExistsError",
+          message: "A user by that username already exists",
+        });
+      } else {
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        const user = await createUser({
+          username,
+          password: hashedPassword,
+          email,
+        });
+
+        const token = jwt.sign(
+          { id: user.id, username: user.username },
+          process.env.JWT_SECRET
+        );
+
+        res.send({ message: "Thank you for signing up!", token });
+      }
     }
-    const _user = await getUserByUsername(username);
-
-    if (_user) {
-      next({
-        name: "UserExistsError",
-        message: "A user by that username already exists",
-      });
-    }
-
-    const user = await createUser({
-      username,
-      password,
-      email,
-    });
-    
-    const token = jwt.sign(
-      {
-        id: user.id,
-        username,
-      },
-      process.env.JWT_SECRET,
-    );
-
-    res.send({
-      message: "thank you for signing up",
-      token,
-    });
   } catch ({ name, message }) {
-    //next({ name, message })
+    next({ name, message });
   }
 });
 
+usersRouter.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await getUserByUsername(username);
+
+    if (!user) {
+      next({
+        name: "UserNotFoundError",
+        message: "User not found",
+      });
+    } else {
+      const passwordMatches = await bcrypt.compare(password, user.password);
+
+      if (passwordMatches) {
+        const token = jwt.sign(
+          { id: user.id, username: user.username },
+          process.env.JWT_SECRET
+        );
+
+        res.send({ message: "You're logged in!", token });
+      } else {
+        next({
+          name: "IncorrectCredentialsError",
+          message: "Username or password is incorrect",
+        });
+      }
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 module.exports = usersRouter;
