0fa113049e2e9cf89224f6fe2e8a630799b7cb0c
Beasted179 <clarios179@gmail.com>
2023-03-02 21:46:08 -0800

    specific error messages for missing multiple params

--- pokeShopBackEnd/api/users.js
+++ pokeShopBackEnd/api/users.js
@@ -4,19 +4,24 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt")
 const { createUser, updateUser, getUserByUsername, getUserByEmail} = require("../db");
 
-usersRouter.use((req, res, next) => {
-  console.log("A request is being made to /users");
-  next(); // THIS IS DIFFERENT
-});
-
 usersRouter.post("/register", async (req, res, next) => {
   const { username, password, email } = req.body;
 
   try {
-    if (!username || !password || !email) {
+    if (!username) {
       next({
         name: "MissingParams",
-        message: "Something is missing",
+        message: "Username is missing",
+      });
+    } else if (!password) {
+      next({
+        name: "MissingParams",
+        message: "Password is missing",
+      });
+    } else if (!email) {
+      next({
+        name: "MissingParams",
+        message: "Email is missing",
       });
     } else {
       const _user = await getUserByUsername(username);
@@ -47,39 +52,6 @@ usersRouter.post("/register", async (req, res, next) => {
         );
 
         res.send({ message: "Thank you for signing up!", token });
-      }
-    }
-  } catch ({ name, message }) {
-    next({ name, message });
-  }
-});
-
-usersRouter.post("/login", async (req, res, next) => {
-  const { username, password } = req.body;
-  
-  try {
-    const user = await getUserByUsername(username);
-
-    if (!user) {
-      next({
-        name: "UserNotFoundError",
-        message: "User not found",
-      });
-    } else {
-      const passwordMatches = await bcrypt.compare(password, user.password);
-
-      if (passwordMatches) {
-        const token = jwt.sign(
-          { id: user.id, username: user.username },
-          process.env.JWT_SECRET
-        );
-
-        res.send({ message: "You're logged in!", token });
-      } else {
-        next({
-          name: "IncorrectCredentialsError",
-          message: "Username or password is incorrect",
-        });
       }
     }
   } catch ({ name, message }) {
