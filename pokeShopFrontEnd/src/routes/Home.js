53bdcf6d9c9e3c9ab411858d68f09c7157003c4f
Henry <henrylai8115@gmail.com>
2023-03-09 20:40:10 -0800

    Added cors and material ui

--- pokeShopFrontEnd/src/routes/Home.js
+++ pokeShopFrontEnd/src/routes/Home.js
@@ -1,13 +1,19 @@
-import React from "react"
 import NavBar from "../components/NavBar"
 import {Outlet} from 'react-router-dom'
 import { useState } from "react"
+
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+
 const Home = () => {
     const [token, setToken] = useState(localStorage.getItem("saved_token"));
     return (
     <div>
         <header>
-            <NavBar token = {token} setToken = {setToken}/>
+            <Stack direction="row" spacing={2} alignItems="center">
+                <Typography margin={1} variant="h6" gutterBottom>Pokefeud</Typography>
+                <NavBar token = {token} setToken = {setToken}/>
+            </Stack>
         </header>
         <main>
             <Outlet context={[token, setToken]}/>
