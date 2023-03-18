1c2ba9f4f2abb5b6167cb0b1b7285aad48f671e5
Henry <henrylai8115@gmail.com>
2023-03-18 14:42:39 -0700

    front merge

--- pokeShopFrontEnd/src/components/cart.js
+++ pokeShopFrontEnd/src/components/cart.js
@@ -5,13 +5,22 @@ const Cart = ()=> {
     const [itemCount, setItemCount] = useState(0)
     const [products, setProducts] = useState({})
     const [isOpen, setIsOpen] = useState(false)
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(anchorEl ? null : event.currentTarget);
+    };
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popper' : undefined;
+
     return(
         <Badge badgeContent={itemCount} color="primary" sx={{margin:"1vh 1.5vw 0 0"}}>
             <IconButton>
                 <ShoppingCartIcon color="action" sx={{margin:"-5px -5px 0 0"}}/>
             </IconButton>
             <Popper
-
+                open={open}
             >
 
             </Popper>
