52f8241c9210150f855d5715c5bbb5d6e8554e81
Old-Lai <89488963+Old-Lai@users.noreply.github.com>
2023-03-19 16:24:32 -0700

    Update stripe.js

--- pokeShopBackEnd/api/stripe.js
+++ pokeShopBackEnd/api/stripe.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require("express");
 const stripeRouter = express.Router();
 const stripe = require('stripe')(process.env.STRIPE_SECRET)
-const DOMAIN = 'http://localhost:3000'; //change this when deploy
+const DOMAIN = 'https://pokefeud-backend.onrender.com/api/stripe'; //change this when deploy
 
 stripeRouter.post('/checkout', async (req, res, next) => {
     try{
